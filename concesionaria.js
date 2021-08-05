994c65182af050811750b402cc867e69ebb5ee50
GrandeJuan <grande.juanf@gmail.com>
2021-08-05 18:45:10 -0300

    Segunda Etapa Terminada

--- concesionaria.js
+++ concesionaria.js
@@ -1,8 +1,17 @@
 const autos = require('./autos.js');
 
 const concesionaria = {
-    funcionalidades: ' ',
-    autos: autos
+    autos: autos,
+    buscarAuto: function (patente) {
+        autosFiltrados = this.autos.filter(function (auto) {
+            return auto.patente == patente
+        })
+        if(autosFiltrados.length == 0){
+            return null
+        }else{
+            return autosFiltrados[0]
+        }
+    }
 };
 
-console.log(concesionaria);
+console.log(concesionaria.buscarAuto('APL123'));
