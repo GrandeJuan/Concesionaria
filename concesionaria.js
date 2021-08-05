9266a95fc61f928b089080b3655768b6c9fc6aab
GrandeJuan <grande.juanf@gmail.com>
2021-08-05 19:35:12 -0300

    Tercer Etapa Terminada

--- concesionaria.js
+++ concesionaria.js
@@ -3,15 +3,23 @@ const autos = require('./autos.js');
 const concesionaria = {
     autos: autos,
     buscarAuto: function (patente) {
-        autosFiltrados = this.autos.filter(function (auto) {
+        const autosFiltrados = this.autos.filter(function (auto) {
             return auto.patente == patente
         })
-        if(autosFiltrados.length == 0){
+        if (autosFiltrados.length == 0) {
             return null
-        }else{
+        } else {
             return autosFiltrados[0]
+        }
+    },
+    venderAuto: function (patente) {
+        const autoFiltrado = this.buscarAuto(patente);
+        if (autoFiltrado) {
+            autoFiltrado.vendido = true
         }
     }
 };
 
-console.log(concesionaria.buscarAuto('APL123'));
+console.log(autos);
+concesionaria.venderAuto('APL123');
+console.log(autos);
