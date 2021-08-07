67ddfe144902c9b2088b4cca645676dcdac4a18d
GrandeJuan <grande.juanf@gmail.com>
2021-08-07 15:46:13 -0300

    Mas Funcionalidades Terminado

--- concesionaria.js
+++ concesionaria.js
@@ -16,22 +16,43 @@ const concesionaria = {
         const autoFiltrado = this.buscarAuto(patente);
         if (autoFiltrado) {
             autoFiltrado.vendido = true
-        }
+        };
     },
     autosParaLaVenta: function () {
         const autosParaVender = this.autos.filter(function (auto) {
             return auto.vendido == false
-        })
+        });
         return autosParaVender
     },
     autosNuevos: function () {
         const listaDeAutosParaVender = this.autosParaLaVenta();
         const autos0Km = listaDeAutosParaVender.filter(function (auto) {
             return auto.km < 100
-        }) 
+        });
         return autos0Km
-        }
-    };
+    },
+    listaDeVentas: function () {
+        const listaDeAutosVendidos = this.autos.filter(function(auto) {
+            return auto.vendido == true
+        });
+        const listaDePreciosNominal = listaDeAutosVendidos.map(function(auto){
+            return auto.precio
+        });
+        return listaDePreciosNominal;
+    }
+}
+
+
+/*     totalDeVentas: function () {
+        const autosVendidos = this.autos.filter(function(auto){
+            return auto.vendido == true
+        })
+        const listaDePreciosAutosVendidos = autosVendidos.reduce(function(acumulador, auto){
+            return acumulador.precio + auto.precio
+        })
+        return listaDePreciosAutosVendidos;
+    } */
+/* }; */
 
 
 /* console.log(autos);
@@ -39,3 +60,5 @@ concesionaria.venderAuto('APL123');
 console.log(autos);
 console.log(concesionaria.autosParaLaVenta());
 console.log(concesionaria.autosNuevos()); */
+console.log(concesionaria.listaDeVentas());
+/* console.log(concesionaria.totalDeVentas()); */
